c7f68d320546210bcbb12e9895e575f31bd7e6b6
Markus Kalkbrenner <git@kalki.de>
2025-11-01 14:04:18 +0100

    Fix variable name for library source directory

--- builder/frameworks/_build_pioasm.py
+++ builder/frameworks/_build_pioasm.py
@@ -7,7 +7,7 @@ from SCons.Script import DefaultEnvironment
 env = DefaultEnvironment()
 
 PROJECT_SRC_DIR = Path(env['PROJECT_SRC_DIR'])
-LIB_SRC_DIRS = Path(env['PROJECT_LIBDEPS_DIR']) / env['PIOENV']
+LIB_SRC_DIR = Path(env['PROJECT_LIBDEPS_DIR']) / env['PIOENV']
 
 # Find the bundled pioasm executable
 pioasm_dir = env.PioPlatform().get_package_dir("tool-pioasm-rp2040-earlephilhower")
@@ -38,4 +38,4 @@ def process_pio_files(src_dir: Path):
 process_pio_files(PROJECT_SRC_DIR)
 
 # Process library source files
-process_pio_files(LIB_SRC_DIRS)
+process_pio_files(LIB_SRC_DIR)
